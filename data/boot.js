f74599ad01858f8f932721093086cc0a4ccec325
thebaconuk <30690370+thebaconuk@users.noreply.github.com>
2017-08-03 20:34:59 +0100

    Comments for Jon
    Added comments for Jon

--- data/boot.js
+++ data/boot.js
@@ -1,11 +1,11 @@
 var boot = function(game) {
     console.log("Initialising Boot");
 
-};
+}; //why does the ; need to be here or does it?
 
-boot.prototype = {
+boot.prototype = { //what is a prototype?
     
-    preload: function() {
+    preload: function() { //if i put game in the () i don't have to put this. in front of everything but it seems to act weird?
             
         this.game.load.tilemap('level0', '/tiled/level0.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tileset', '/assets/tileset.png');
@@ -14,7 +14,7 @@ boot.prototype = {
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.pageAlignVertically = true;
-                
+        //this. IS DRIVING ME INSANE!!?? WHAT IS THIS.? LOL        
     },
 
     create: function() {
